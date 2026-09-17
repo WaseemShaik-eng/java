3bc00a1e230b7187692ca11b3c613ec8a73c553c
Waseem.Shaik <waseemshaik172@gmail.com>
2026-09-17 07:19:30 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -4,3 +4,22 @@ class Hello{
      System.out.println("Hello Github");
       }
   }
+
+
+import java.util.Scanner;
+class Operators{
+  public static void main(String[] args){
+    Scanner sc= new Scannner(System.in);
+    System.out.println("Enter a and b values");
+    int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Addition",a+b);
+        System.out.println("Subtraction",a-b);
+        System.out.println("multiplication",a*b);
+        System.out.println("division",a/b);
+        System.out.println("modulus",a%b);
+    sc.close();
+  }
+}
+
+    
