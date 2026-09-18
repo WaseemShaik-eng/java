f21b3673c7c7eb5ca8d9ebeac143eb12d84b05f1
Waseem.Shaik <waseemshaik172@gmail.com>
2026-09-18 06:22:20 +0530

    Update basic.java
    Fixed Scanner instantiation and corrected output formatting for arithmetic operations.

--- basic.java
+++ basic.java
@@ -2,23 +2,29 @@ class Hello{
   public static void main(String[] args){
   System.out.println("Hello World");
      System.out.println("Hello Github");
+       System.out.println("Hello ai");
       }
   }
 
+// all arithematic operators
 
 import java.util.Scanner;
-class Operators{
+class Main{
   public static void main(String[] args){
-    Scanner sc= new Scannner(System.in);
-    System.out.println("Enter a and b values");
-    int a=sc.nextInt();
-        int b=sc.nextInt();
-        System.out.println("Addition",a+b);
-        System.out.println("Subtraction",a-b);
-        System.out.println("multiplication",a*b);
-        System.out.println("division",a/b);
-        System.out.println("modulus",a%b);
-    sc.close();
+      Scanner sc= new Scanner(System.in);
+
+      System.out.println("Enter a and b values:");
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+
+      System.out.println("Addition:" + (a+b));
+      System.out.println("Subtraction:" + (a-b));
+
+      System.out.println("multiplication:"+ (a*b));
+      
+      System.out.println("division:"+ (a/b));
+      System.out.println("modulus:"+ (a%b));
+      
   }
 }
 
