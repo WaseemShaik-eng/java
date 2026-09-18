b4de16b2a243db618d8a74f695d880dc9ae2baad
Waseem.Shaik <waseemshaik172@gmail.com>
2026-09-18 07:09:34 +0530

    Update control stmt.java

--- control stmt.java
+++ control stmt.java
@@ -16,3 +16,69 @@ class Main{
     System.out.println("Not Eligible for vote");
   }
 }
+
+// calculator using switch
+
+import java.util.Scanner;
+class Main{
+  public static void main(String[] args){
+    Scanner sc =new Scanner(System.in);
+  
+    System.out.println("Enter a and b values:");
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    System.out.println("Enter your choice:");
+    int choice=sc.nextInt();
+    switch(choice){
+       
+    case 1:System.out.println(a+b);
+            break;
+            case 2:System.out.println(a-b);
+            break;
+            case 3:System.out.println(a*b);
+            break;
+            case 4:System.out.println(a/b);
+            break;
+            case 5:System.out.println(a%b);
+            break;
+            default:
+                  System.out.println("Invaild");
+            }
+    }
+  }
+
+
+// Even or odd number
+import java.util.Scanner;
+class Main{
+
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+
+    System.out.println("Enter a number:");
+    int a=sc.nextInt();
+
+    if(a%2==0)
+    System.out.println("Even Number");
+    else
+    System.out.println("Odd Number");
+  }
+}
+
+
+// Prime number or not
+import java.util.Scanner;
+class Main{
+
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+
+    System.out.println("Enter a number:");
+    int a=sc.nextInt();
+
+    if(a%2!=0)
+    System.out.println("Prime Number");
+    else
+    System.out.println("Not a prime Number");
+  }
+}
