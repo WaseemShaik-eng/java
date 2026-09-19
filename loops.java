aca6cdfb49ab804a28f8606851dc97dafa7b8bad
Waseem.Shaik <waseemshaik172@gmail.com>
2026-09-19 06:18:09 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,44 @@
+// find factorial of n numbers
+import java.util.*;
+
+class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    
+    System.out.println("Enter the n value:");
+    int n=sc.nextInt();
+   
+    int i;
+    int fact =1;
+    
+    for(i=1;i<=n;i++){
+      fact=fact*i;
+    }
+    System.out.println(fact);
+  }
+}
+
+
+import java.util.*;
+ 
+ class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter n value:");
+    int n =sc.nextInt();
+
+    for(int i=0;i<=n;i++){
+       for(int j=1;j<=i;j++){
+
+         System.out.print(j+" ");
+       }
+
+         System.out.println();
+    }
+          System.out.println();
+    
+    
+    
+  }
+ }
